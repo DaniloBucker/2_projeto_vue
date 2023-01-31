671cfdbdf68dd7c335735b05b744f5a8330a6f40
DaniloBucker <danilobucker@gmail.com>
2023-01-31 17:30:10 -0300

    primeiro commit
    tudo já veio assim no arquivo.
    nada modificado por mim.

---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Meu nome é: {{ nome }}</h1>
+</template>
+<script>
+   export default {
+       name: 'LifeCycle',
+       data() {
+           return {
+               nome: 'Ainda nao sei'
+           }
+       } 
+   }
+</script>
